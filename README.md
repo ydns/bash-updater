6aa78f7c7aac1f7edc5db043e0dd40b100c59991
Christian Jurk <commx@commx.ws>
2013-05-21 23:00:12 +0200

    Added API description and the updater script

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-ydns
-====
+yDNS update API
+---------------
 
-Tools for the yDNS service
+To update a yDNS host, you have to do simple HTTP GET requests. Each 
+request must use HTTP authorization in order to perform authorization of 
+your request:
+
+The URL to perform update calls is:
+http://ydns.eu/api/v1/update/?host=myhost.ydns.eu[&ip=ip_address]
+
+where the host parameter is required and the ip parameter is optional. 
+If the latter is not specified, the public IP address as seen on the 
+Internet is being used (should be fine for most situations). The IP 
+address can be either an IPv4 or IPv6 address. You have to use the IP 
+parameter if you want to update your IPv6 address, because the yDNS 
+server currently does not have a public IPv6 address configured.
+
+When requesting a host update, the server will respond either:
+
+* HTTP status 400 (BAD REQUEST) if something is wrong with your parameters.
+* HTTP status 401 (UNAUTHORIZED) if the authentication is invalid.
+* HTTP STATUS 404 (NOT FOUND) if the specified host is not found.
+* HTTP STATUS 200 (OK) if the update was successful.
---
+++ updater.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# yDNS Updater, updates your yDNS host.
+# Copyright (C) 2013 Christian Jurk <cj@ydns.eu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+##
+# Define your yDNS account details and host you'd like to update.
+##
+
+YDNS_USER="user@host.xx"
+YDNS_PASSWD="secret"
+YDNS_HOST="myhost.ydns.eu"
+
+##
+# Don't change anything below.
+##
+if ! hash curl 2>/dev/null; then
+	echo "ERROR: cURL is missing."
+	exit 1
+fi
+
+ret=`curl --basic \
+	-u "$YDNS_USER:$YDNS_PASSWD" \
+	--silent \
+	http://ydns.eu/api/v1/update/?host=$YDNS_HOST`
+
+if [ "$ret" = "ok" ]; then
+	echo "Update: OK"
+	exit 0
+else
+	echo "Update failed: $ret"
+	exit 1
+fi
